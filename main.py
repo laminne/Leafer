6f60be122a39fb253bebd88f426e381115191aec
Laminne Tatsuto Yamamoto <laminne33569@approvers.dev>
2020-08-14 23:02:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,19 +7,13 @@ client = discord.Client()
 conn = sqlite3.connect('data.db')
 c = conn.cursor()
 
-async def send(channel,*args, **kwargs): return await channel.send(*args, **kwargs)
- 
 @client.event
 async def on_message(message):
     if message.author.bot:
         return
-    # if message.content == '':
-    #     def check(command):
-    #         return command.author == message.author
-    #     cc = await client.wait_for("message", check=check)
     uname = message.author.id
     con = message.content
-    sql = 'insert into data (username, message) values (?,?)'
+    sql = 'insert into data (userid, message) values (?,?)'
     namelist = (uname, con)
     c.execute(sql, namelist)
     conn.commit()
